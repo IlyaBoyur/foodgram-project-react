79fe89c53015ab014376e74a059f78bac5bbdc6d
Ilya Boyur <ilya.boyur.r@ya.ru>
2021-09-22 20:55:42 +0300

    Add User routes tests

---
+++ backend/foodgram/api/tests/test_routes.py
@@ -0,0 +1,19 @@
+from django.urls import reverse
+
+
+def test_routes(setup_user):
+    """URL-адрес, рассчитанный через имя,
+    соответствует ожидаемому видимому URL."""
+    routes = {
+        # Static URLs
+        '/api/auth/token/login/': reverse('token_login'),
+        '/api/auth/token/logout/': reverse('token_logout'),
+        '/api/users/': reverse('users-list'),
+        '/api/users/me/': reverse('users-me'),
+        '/api/users/set_password/': reverse('users-set-password'),
+        # Non static generated URLs
+        f'/api/users/{setup_user.id}/': reverse('users-detail',
+                                                args=[setup_user.id]),
+    }
+    for url, reversed_url in routes.items():
+        assert url == reversed_url
