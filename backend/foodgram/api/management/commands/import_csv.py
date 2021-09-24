0fe3abbe3367a01207a29683cd595d4ab906db21
Ilya Boyur <ilya.boyur.r@ya.ru>
2021-09-24 19:59:21 +0300

    Add manage command to import csv with progress bar

---
+++ backend/foodgram/api/management/commands/import_csv.py
@@ -0,0 +1,39 @@
+import csv
+
+import django.db.utils
+from django.core.management.base import BaseCommand, CommandError
+
+from api.models import Ingredient
+from tqdm import tqdm
+
+DATA = (
+    ('../data/ingredients.csv', ('name', 'measurement_unit'), Ingredient),
+)
+
+
+class Command(BaseCommand):
+    help = 'Imports data from csv files to database'
+
+    def handle(self, *args, **options):
+        self.stdout.write(
+            self.style.NOTICE('Please wait until Finished! prompt')
+        )
+        for file, fieldnames, Model in DATA:
+            with open(file, encoding='utf8') as csvfile:
+                total = sum(1 for _ in csv.reader(csvfile))
+                csvfile.seek(0)
+                for row in tqdm(csv.DictReader(csvfile, fieldnames=fieldnames),
+                                total=total):
+                    try:
+                        Model.objects.create(**row)
+                    except django.db.utils.IntegrityError:
+                        self.stdout.write(self.style.ERROR(
+                            f'Unable to create {Model.__name__}'
+                            f' from {file} with values: {*row.values(),}' 
+                        ))
+                        total -= 1
+                self.stdout.write(self.style.SUCCESS(
+                    f'Successfully created {total} {Model.__name__}'
+                    f' from {file}'
+                ))
+        self.stdout.write(self.style.SUCCESS('Finished!'))
