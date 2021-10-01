f0251b6eb39f4b099a5b6062ed60cab03250c8ba
Ilya Boyur <ilya.boyur.r@ya.ru>
2021-10-01 14:16:38 +0300

    Remove unused user serializers

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -16,36 +16,6 @@ class UserReadSerializer(serializers.ModelSerializer):
                   'is_subscribed')
 
 
-class UserWriteSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = '__all__'
-
-
-class UserCreateSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
-
-    class Meta:
-        model = User
-        fields = ('email', 'id', 'username', 'first_name', 'last_name',
-                  'password')
-
-    def validate(self, attrs):
-        user = User(**attrs)
-        password = attrs.get("password")
-        try:
-            validate_password(password, user)
-        except django_exceptions.ValidationError as error:
-            raise serializers.ValidationError(
-                {"password": serializers
-                             .as_serializer_error(error)["non_field_errors"]}
-            )
-        return attrs
-
-    def create(self, validated_data):
-        return User.objects.create_user(**validated_data)
-
-
 class IngredientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ingredient
