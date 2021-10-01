c96f6422956f90c3a2dc6a7f4ddc5962c806f09c
Ilya Boyur <ilya.boyur.r@ya.ru>
2021-10-01 18:46:00 +0300

    Add recipe filters

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -1,5 +1,15 @@
 import django_filters as filters
+from django.contrib.auth import get_user_model
 from django_filters.rest_framework import FilterSet
+
+from .models import Tag
+
+BOOLEAN_CHOICES = (
+    (0, 'False'),
+    (1, 'True'),
+)
+
+User = get_user_model()
 
 
 class IngredientFilter(FilterSet):
@@ -7,4 +17,12 @@ class IngredientFilter(FilterSet):
 
 
 class RecipeFilter(FilterSet):
-    pass
+    is_favorited = filters.ChoiceFilter(choices=BOOLEAN_CHOICES)
+    is_in_shopping_cart = filters.ChoiceFilter(choices=BOOLEAN_CHOICES)
+    author = filters.ModelChoiceFilter(queryset=User.objects.all())
+    tags = filters.ModelMultipleChoiceFilter(
+        field_name='tags__slug',
+        to_field_name='slug',
+        conjoined=True,
+        queryset=Tag.objects.all(),
+    )
