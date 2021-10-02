901e76e2bb9330b6f8b91107616da11a5b4c9cd2
Ilya Boyur <ilya.boyur.r@ya.ru>
2021-10-03 00:36:03 +0300

    Update workflow: fix security key issue on server

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -9,12 +9,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-sb7#phb)2_l#k+h-o41klqwz-(=k%qk4w-z2#8taf4q3plb22_'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
-# DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '84.201.137.25']
 # ALLOWED_HOSTS = []
