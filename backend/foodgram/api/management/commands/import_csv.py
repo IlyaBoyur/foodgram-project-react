d0fd4c9e2ca20c0ae1e2c060d024ecedc4c62ce5
Ilya Boyur <ilya.boyur.r@ya.ru>
2021-10-04 12:50:56 +0300

    Move variable text from code to const: import_csv

--- backend/foodgram/api/management/commands/import_csv.py
+++ backend/foodgram/api/management/commands/import_csv.py
@@ -6,6 +6,13 @@ from tqdm import tqdm
 
 from api.models import Ingredient
 
+MESSAGE_START = 'Please wait until Finished! prompt'
+MESSAGE_ERROR_CREATE = (
+    'Unable to create {model} from {file} with values: {values}'
+)
+MESSAGE_SUCCESS_CREATE = 'Successfully created {total} {model} from {file}'
+MESSAGE_FINISH = 'Finished!'
+
 DATA = (
     ('../data/ingredients.csv', ('name', 'measurement_unit'), Ingredient),
 )
@@ -16,7 +23,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         self.stdout.write(
-            self.style.NOTICE('Please wait until Finished! prompt')
+            self.style.NOTICE(MESSAGE_START)
         )
         for file, fieldnames, Model in DATA:
             with open(file, encoding='utf8') as csvfile:
@@ -28,12 +35,18 @@ class Command(BaseCommand):
                         Model.objects.create(**row)
                     except django.db.utils.IntegrityError:
                         self.stdout.write(self.style.ERROR(
-                            f'Unable to create {Model.__name__}'
-                            f' from {file} with values: {*row.values(),}'
+                            MESSAGE_ERROR_CREATE.format(
+                                model=Model.__name__,
+                                file=file,
+                                values=(*row.values(),)
+                            )
                         ))
                         total -= 1
                 self.stdout.write(self.style.SUCCESS(
-                    f'Successfully created {total} {Model.__name__}'
-                    f' from {file}'
+                    MESSAGE_SUCCESS_CREATE.format(
+                        total=total,
+                        model=Model.__name__,
+                        file=file,
+                    )
                 ))
-        self.stdout.write(self.style.SUCCESS('Finished!'))
+        self.stdout.write(self.style.SUCCESS(MESSAGE_FINISH))
