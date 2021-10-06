bc0a9013d0c1cb1894ef4836677164320b7961a7
Ilya Boyur <ilya.boyur.r@ya.ru>
2021-10-06 19:29:23 +0300

    Fix bug in filtering for Recipe

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -4,6 +4,11 @@ from django_filters.rest_framework import FilterSet
 
 from .models import Tag
 
+BOOLEAN_TO_RANGE = {
+    False: 'exact',
+    True: 'gt',
+}
+
 User = get_user_model()
 
 
@@ -12,11 +17,16 @@ class IngredientFilter(FilterSet):
 
 
 class RecipeFilter(FilterSet):
-    is_favorited = filters.BooleanFilter()
-    is_in_shopping_cart = filters.BooleanFilter()
+    is_favorited = filters.BooleanFilter(method='filter_bool_to_range')
+    is_in_shopping_cart = filters.BooleanFilter(method='filter_bool_to_range')
     author = filters.ModelChoiceFilter(queryset=User.objects.all())
     tags = filters.ModelMultipleChoiceFilter(
         field_name='tags__slug',
         to_field_name='slug',
         queryset=Tag.objects.all(),
     )
+
+    def filter_bool_to_range(self, queryset, name, value):
+        """Select number fieeld differently for `false` and for 'true'"""
+        lookup = '__'.join([name, BOOLEAN_TO_RANGE[value]])
+        return queryset.filter(**{lookup: 0})
