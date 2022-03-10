c7b3373da44d1d82592dcf6da58d0e6b1257b077
Ilya Boyur <ilya.boyur.r@ya.ru>
2022-03-10 14:00:05 +0300

    Fix security issues

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -4,8 +4,8 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
-DEBUG = False
-ALLOWED_HOSTS = ['127.0.0.1', '84.201.137.25']
+DEBUG = (os.environ.get('DJANGO_DEBUG_VALUE') == 'True')
+ALLOWED_HOSTS = ['127.0.0.1', 'www.foodgram-project.ml']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
